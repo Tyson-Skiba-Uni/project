676a64bdc12cbebd002da79ceb84bdbeff4d7447
Tyson-Skiba-Uni <tyson.skiba@live.vu.edu.au>
2016-03-14 10:38:53 +0000

    Test from New Drive New OS

--- project.c
+++ project.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-	printf("Echo to Terminal Screen\n");
+	printf("Echo to Terminal Screen: Using Newly Compiled Machine\n");
 	printf("First Addition\n\n");
 	printf("SSH Protacol\n");
 	return 0;
