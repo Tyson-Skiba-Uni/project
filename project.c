b42ae74c797f203136d15c9fe5652a8cf9278650
Tyson-Skiba-Uni <tyson.skiba@live.vu.edu.au>
2016-03-20 22:55:37 +0000

    This is For Homework Week2

--- project.c
+++ project.c
@@ -4,5 +4,6 @@ int main(void){
 	printf("Echo to Terminal Screen: Using Newly Compiled Machine\n");
 	printf("First Addition\n\n");
 	printf("SSH Protacol\n");
+	printf("NewLine for Homework\n\n\n\n");
 	return 0;
 }
